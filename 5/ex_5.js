a35b5dc90508019367e4e00a54242da5ea354b3b
mia336love <122865270+mia336love@users.noreply.github.com>
2023-02-16 02:39:13 +0300

    completed ex_5

--- 5/ex_5.js
+++ 5/ex_5.js
@@ -1,1 +1,11 @@
 // Write a JavaScript conditional statement to sort ten numbers.
+
+let arr = [15, 45, 1, 23, 99, 87, 6, 18, 2, 37];
+
+function sortArr(a, b) {
+  if (a > b) return 1;
+  if (a == b) return 0;
+  if (a < b) return -1;
+}
+
+console.log(arr.sort(sortArr));
