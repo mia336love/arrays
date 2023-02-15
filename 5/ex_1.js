0862ef4538b25259953b9abe66cabf4cdea92997
mia336love <122865270+mia336love@users.noreply.github.com>
2023-02-16 00:49:16 +0300

    Update ex_1.js

--- 5/ex_1.js
+++ 5/ex_1.js
@@ -1,7 +1,5 @@
 // Напишите программу на JavaScript, которая принимает десять целых чисел как массив и отображает большее
 
-//numbers = [2, 8, 4, 6, 12, -12, -4, -6, -2, -8];
-
 numbers = 2846139057;
 let myFunc = (num) => Number(num);
 let arr = Array.from(String(numbers), myFunc);
