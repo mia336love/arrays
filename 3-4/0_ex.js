ae66fd41fe7e6e88d7a2f43a456f468112c69d0b
mia336love <122865270+mia336love@users.noreply.github.com>
2023-02-15 18:42:47 +0300

    Update 0_ex.js

--- 3-4/0_ex.js
+++ 3-4/0_ex.js
@@ -1,1 +1,8 @@
-let styles = [];
+let styles = ["Джаз", "Блюз"];
+
+styles.push("Рок-н-ролл");
+styles.splice(1, 1, "Классика");
+console.log(styles.shift());
+styles.unshift("Рэп", "Регги");
+
+console.log(styles);
